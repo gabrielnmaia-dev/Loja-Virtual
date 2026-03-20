6dac1ade41fd9ab6deafb86e6675d196e2c58b39
gabrielnmaia-dev <gbg292526@gmail.com>
2026-03-20 18:55:38 -0300

    CRUD list crate update e delete

--- core/views.py
+++ core/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Produto
 # Create your views here.
+
+
+class ProdutoListView(ListView):
+    model = Produto
+    template_name = 'produtos/lista.html'
+    context_object_name = 'produtos'
+
+class ProdutoCreateView(CreateView):
+    model = Produto
+    fields = ['nome','preco','categorias','descricao','imagem']
+    template_name = 'produtops/form.html'
+    success_url = reverse_lazy('lista_produtos')
+
+class ProdutoUpdateView(UpdateView):
+    model = Produto
+    fields = ['nome','preco','categorias','descricao','imagem']
+    template_name = 'produtos/form.html'
+    success_url = reverse_lazy('lista_produtos')
+
+class ProdutoDeleteView(DeleteView):
+    model = Produto
+    template_name = 'produtos/confirm_delete.html'
+    success_url = reverse_lazy('lista_produtos')
