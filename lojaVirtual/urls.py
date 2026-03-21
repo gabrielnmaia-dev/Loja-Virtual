7ddef9706067852b4caff837fd9a3913cc5764eb
gabrielnmaia-dev <gbg292526@gmail.com>
2026-03-21 11:32:27 -0300

    CRUD list crate update delete e urls corrigidas

--- lojaVirtual/urls.py
+++ lojaVirtual/urls.py
@@ -19,7 +19,8 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('produtos.urls')),
+    path('', include('core.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
